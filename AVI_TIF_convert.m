5d4fad73827b68fd0269efd5c81fca3bffb9ae6a
mbaxip <madhurabaxi@gmail.com>
2016-11-08 15:55:39 -0500

    Update AVI_TIF_convert.m

--- AVI_TIF_convert.m
+++ AVI_TIF_convert.m
@@ -2,6 +2,8 @@ function [] = AVI_TIF_convert(avi_filename)
 %% converts AVI files to tiff stack files
 %% Usage: AVI_TIF_convert(avi_filename)
 % example: AVI_TIF_convert('simulateCalcImg.avi')
+% author: Madhura Baxi
+% Date: 8th Nov, 2016
 obj = VideoReader(avi_filename);
 vid = read(obj);
 frames = obj.NumberOfFrames;
