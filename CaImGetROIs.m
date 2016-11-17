5376b6bdfa6e5e8df05608d1ce471714f4412bde
Janis <jintoy@bu.edu>
2016-11-17 07:55:02 -0500

    change cutoff back to .5

--- CaImGetROIs.m
+++ CaImGetROIs.m
@@ -164,7 +164,7 @@ end
 % figure();imagesc(filter2(h,summedCrossCorr,'same'));
 
 
-cutoff = .3;
+cutoff = .5;
 % full version of cross-correlation adjacency matrix
 % get all possible cross-correlations between nearby pixels
 %  linear indexing to row-column indexing
