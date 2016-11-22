0f8d144cf627aa6c8488425997631b499efe492c
Janis <jintoy@bu.edu>
2016-11-22 11:25:37 -0500

    update plots for batch test

--- batchTestOnSimulation.m
+++ batchTestOnSimulation.m
@@ -56,31 +56,67 @@ for s = 1:length(snrs)
 end
 
 %% pixels that get through thresholding
+sym = '+o*pxsd^v<>h';
 figure(1); clf; hold on;
+for r = 1:length(nROIs)
+    plot(-1, -1, sym(r)); 
+end
 for r = 1:length(nROIs)
     hitPixels = vertcat(combo(r, :).hitPixels);
     faPixels = vertcat(combo(r, :).faPixels);
     
-    % i'd prefer a 2d errorbar but :P
-    plot(faPixels(:, 1), hitPixels(:, 1), 'o-');
+    %plot(faPixels(:, 1), hitPixels(:, 1), 'o-');
+    surface([faPixels(:, 1), faPixels(:, 1)],...
+        [hitPixels(:, 1), hitPixels(:, 1)],...
+        zeros(length(snrs), 2),...
+        [snrs', snrs'],...
+        'facecol', 'no',...
+        'edgecol', 'interp',...
+        'linewidth', 2,...
+        'marker', sym(r),...
+        'markersize', 10);
 end
 xlabel('false alarm rate');
 ylabel('hit rate');
 title('step 1: identifying pixels with signal');
 legend(cellfun(@(x) [num2str(x), ' ROI'], num2cell(nROIs),...
     'UniformOutput', false), 'Location', 'southeast');
+xlim([0, 1]);
+ylim([.95, 1]);
+hcb = colorbar;
+title(hcb, 'SNR');
+set(hcb, 'Ticks', snrs, 'TickLabels', cellfun(@num2str, num2cell(snrs), ...
+    'UniformOutput', false));
 
 %% pixels that get through clustering
 figure(2); clf; hold on;
+for r = 1:length(nROIs)
+    plot(-1, -1, sym(r)); 
+end
 for r = 1:length(nROIs)
     hitSignal = vertcat(combo(r, :).hitSignal);
     faSignal = vertcat(combo(r, :).faSignal);
     
     % i'd prefer a 2d errorbar but :P
-    plot(faSignal(:, 1), hitSignal(:, 1), 'o-');
+    %plot(faSignal(:, 1), hitSignal(:, 1), 'o-');
+    surface([faSignal(:, 1), faSignal(:, 1)],...
+        [hitSignal(:, 1), hitSignal(:, 1)],...
+        zeros(length(snrs), 2),...
+        [snrs', snrs'],...
+        'facecol', 'no',...
+        'edgecol', 'interp',...
+        'linewidth', 2,...
+        'marker', sym(r),...
+        'markersize', 10);
 end
 xlabel('false alarm rate');
 ylabel('hit rate');
 title('step 2: pixels that get clustered');
 legend(cellfun(@(x) [num2str(x), ' ROI'], num2cell(nROIs),...
     'UniformOutput', false), 'Location', 'southeast');
+xlim([0 .25]);
+ylim([.2 1]);
+hcb = colorbar;
+title(hcb, 'SNR');
+set(hcb, 'Ticks', snrs, 'TickLabels', cellfun(@num2str, num2cell(snrs), ...
+    'UniformOutput', false));
