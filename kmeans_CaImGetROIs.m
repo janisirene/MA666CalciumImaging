21d82cc4a3b65a777f79e57adf6b4df01c7f5c63
Byron Price <byron.h.price@gmail.com>
2016-12-05 15:05:29 -0500

    Add more future steps

--- kmeans_CaImGetROIs.m
+++ kmeans_CaImGetROIs.m
@@ -17,21 +17,19 @@ function [detectedROI,finalBinaryImage] = kmeans_CaImGetROIs(filename,estNeuronR
 %    misssed the first time
 %
 % INPUT: filename - .avi or .tif filename as a string, or matlab array
-%          (width-by-height- number of frames)
+%          (width-by-height-by-number of frames)
 %        estNeuronRadius - radius of ROI in pixels (defaults to 5)
 %        maxNeurons - maximum number of neurons in field of view (defaults
 %         to 20) 
-%        tolerance - for x-means clustering algorithm, try a range of
-%         values (defaults to 0.1)
-% OUTPUT: idealIdx - a vector containing the neuron index for each pixel
-%           considered to have significant cross-correlation structure with
-%           its neighbors
-%         allPixels - a vector containing the image index for each pixel
-%           included above
+% OUTPUT: detectedROI - a struct array containing pixel indeces for the
+%          identified ROIs across various values of a parameter called
+%          tolerance
+%         finalBinaryImage - a binary image that initializes the search for
+%          ROIs, it's effectively: potential signal = 1 and noise = 0
 % 
 %Created: 2016/11/18
 % Byron Price
-%Updated: 2016/12/04
+%Updated: 2016/12/05
 %  By: Byron Price
 
 % Future steps:
@@ -42,6 +40,18 @@ function [detectedROI,finalBinaryImage] = kmeans_CaImGetROIs(filename,estNeuronR
 %  2) remove non-stationary background in time and space ... in many
 %      videos, especially single-photon ones, the noise appears to be highly
 %      correlated with the signal, which would break this algorithm
+%  3) try different initializations to find signal pixels ... different
+%      types of projection images (standard deviation, maximum brightness,
+%      maximum auto-correlation, maximum partial auto-correlation, or
+%      combinations thereof) ... you could calculate a weighted sum of all
+%      those and find the metric that provides the greatest separation
+%      between signal pixels and noise pixels
+%  4) figure out how to punish the distance metric in the k-means algorithm
+%      so that two pixels significantly separated in Euclidean space do not form
+%      part of the same cell and so that each ROI is one connected component 
+%      ... right now the distance metric is a correlation metric and the 
+%      euclidean distance between pixels is ignored
+%  http://www.nature.com/articles/srep08143  for partial cross-correlation
 
 if nargin < 2
     estNeuronRadius = 5;
@@ -49,7 +59,6 @@ if nargin < 2
 elseif nargin < 3
     maxNeurons = 20;
 end
-
 
 estNeuronRadius = round(estNeuronRadius);
 estNeuronArea = pi*estNeuronRadius*estNeuronRadius;
@@ -69,6 +78,7 @@ if ischar(filename) % data input is .avi or .tif or matlab array
                 fullVideo(:,:,count) = mean(double(temp),3);
                 count = count+1;
             end
+            %fullVideo = fullVideo(60:470,230:640,:);
         case '.tif'
             fullVideo = double(readTifStack(filename));
             numFrames = size(fullVideo, 3);
@@ -78,8 +88,11 @@ else % data input is already 3d array
     numFrames = size(fullVideo, 3);
 end
 
-% spatial downsample ... consider temporal as well
-fullVideo = fullVideo(1:2:end,1:2:end,:);
+minVal = min(min(min(fullVideo)));
+maxVal = max(max(max(fullVideo)));
+fullVideo = (fullVideo-minVal)./(maxVal-minVal);
+% consider spatial downsample ... maybe temporal as well
+% fullVideo = fullVideo(1:2:end,1:2:end,:);
 
 % denoise each frame separately with Wiener filter and 
 %  calculate a "maximum brightness composite image" 
@@ -92,18 +105,39 @@ fltVideo = zeros(size(fullVideo));
 for ii=1:numFrames
     temp = fullVideo(:,:,ii);
     fltVideo(:,:,ii) = wiener2(temp,[round(estNeuronRadius/2),round(estNeuronRadius/2)]);
-    pixelValues = fltVideo(:,:,ii);threshold = quantile(pixelValues(:),0.999);
+    pixelValues = fltVideo(:,:,ii);threshold = quantile(pixelValues(:),0.99);
     maxBrightIm = maxBrightIm+(pixelValues>threshold);
 end
-maxBrightIm = maxBrightIm>0;
+maxBrightIm = maxBrightIm>2;
+% figure();imagesc(maxBrightIm);
+
 %implay(uint8(fltVideo));
+
+% % maximum first-derivative projection
+% maxFirstDeriv = zeros(width,height);
+% sampleFreq = 30; %fps 
+% for ii=1:width
+%     for jj=1:height
+%         temp = zeros(length(2:numFrames-1),1);
+%         for kk=2:numFrames-1
+%             temp(kk-1) = (fltVideo(ii,jj,kk+1)-fltVideo(ii,jj,kk-1)).*sampleFreq;
+%         end
+%         maxFirstDeriv(ii,jj) = max(temp);
+%     end
+% end
+% figure();imagesc(maxFirstDeriv);
 
 display('Calculating cross-correlations...');
 % initialize search for ROIs by taking cross-correlations, 
 %  we look for pixels with significant auto-correlations and 
 %  cross-correlations with neighboring pixels
+%  this is very slow and does not work well on real data, consider
+%  initializing the search with some kind of maximum brightness projection,
+%  a standard deviation projection, the above maximum first derivative
+%  projection, or a maximum partial auto- or cross- correlation image
 
 % This is a spatial-average cross-correlation
+% parCorrImage = zeros(width,height);
 summedCrossCorr = zeros(width,height);
 divisor = zeros(width,height);
 maxlag = 5;
@@ -111,6 +145,8 @@ for ii=1:width
     for jj=1:height
         % take cross-correlations in a small window around each pixel in
         %  the image
+%         temp = parcorr(squeeze(fltVideo(ii,jj,:)),maxlag);
+%         parCorrImage(ii,jj) = max(abs(temp(2:end)));
         rowVec = max(1, ii-estNeuronRadius):min(width, ii+estNeuronRadius);
         colVec = max(1, jj-estNeuronRadius):min(height, jj+estNeuronRadius);
         for kk=rowVec
@@ -123,10 +159,14 @@ for ii=1:width
         end
     end
 end
-summedCrossCorr = summedCrossCorr./divisor;
-%figure();imagesc(summedCrossCorr);title('Spatial Average Maximum Cross-Correlation Image');
+% figure();imagesc(parCorrImage);
 
-% for thresholding the cross-correlation coefficients
+summedCrossCorr = summedCrossCorr./divisor;
+% figure();imagesc(summedCrossCorr);title('Spatial Average Maximum Cross-Correlation Image');
+
+% % for thresholding the cross-correlation coefficients
+% works well on simulated data, poorly on real data, need to understand
+% statistic of noise better
 temp = summedCrossCorr(:);
 GMModel = fitgmdist(temp,2); % fit a 2-component Gaussian mixture model for 
              % the set of cross-correlation coefficients ...
@@ -135,8 +175,8 @@ GMModel = fitgmdist(temp,2); % fit a 2-component Gaussian mixture model for
 mu = GMModel.mu;Sigma = GMModel.Sigma;
 [~,minInd] = min(mu);
 
-% thresholding creates a binary image, which shows pixels in the field
-%  of view that might belong to cells
+% % thresholding creates a binary image, which shows pixels in the field
+% %  of view that might belong to cells
 threshold = mu(minInd)+3*sqrt(Sigma(minInd));
 tempBinaryImage  = summedCrossCorr > threshold;
 
@@ -176,13 +216,16 @@ for kk=1:maxNeurons
             mean(squeeze(fltVideo(r(ll),c(ll),:))');
     end
     % correlation is the metric, as pixels within the same cell should be
-    %  highly correlated
+    %  highly correlated, need to figure out how to punish for pixels that
+    %  are far away and punish for ROIs that are not connected into one
+    %  component
     [idx,~,SUMD] = kmeans(X,kk,'Distance','correlation','Replicates',10);
     bigSum(kk) = sum(SUMD);
     tempPixelIdxList{kk} = idx;
 end
 difference = diff(-bigSum);
 
+figure();plot(bigSum,'LineWidth',2);
 % try to find the "elbow" of the bigSum vector ... plot bigSum to see that
 %  it looks a bit like an AIC curve ... it's the sum of the distances
 %  between every pixel and the centroid of the cell to which it belongs ...
@@ -195,13 +238,10 @@ tolerance = [1e-2,1e-1,1e0,1e1,1e2];
 detectedROI = cell(length(tolerance), 1);
 for t=1:length(tolerance)
     minInd = find(difference<tolerance(t),1,'first');
-    
     if isempty(minInd) == 1
-        idealComponents = length(bigSum);
-    elseif minInd == 1
-        idealComponents = minInd;
+        idealComponents = maxNeurons;
     else
-        idealComponents = minInd-1;
+        idealComponents = minInd;
     end
     idealIdx = tempPixelIdxList{idealComponents};
 
@@ -216,15 +256,16 @@ for t=1:length(tolerance)
         end
     end
     detectedROI{t} = tempROI;
+    
+%     figure();hold on;
+%     for ii=1:idealComponents
+%         pixels = allPixels(idealIdx==ii);
+%         [r,c] = ind2sub([width,height],pixels);
+%         plot(r,c,'.','MarkerSize',10);
+%     end
+%     set(gca,'YDir','reverse');title(sprintf('%d Identified Cells',idealComponents));
 end
 
-% figure();hold on;
-% for ii=1:idealComponents
-%     pixels = allPixels(idealIdx==ii);
-%     [r,c] = ind2sub([width,height],pixels);
-%     plot(r,c,'.','MarkerSize',10);
-% end
-% set(gca,'YDir','reverse');title(sprintf('%d Identified Cells',idealComponents));
 end
 
 function [crossCorr] = myXCORR(Signal1,Signal2,numLags)
