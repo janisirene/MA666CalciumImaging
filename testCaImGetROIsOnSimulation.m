406c830d20823dcefdaf75456db5dd99a8918860
Janis <jintoy@bu.edu>
2016-12-12 19:48:02 -0500

    add comments to test function

--- testCaImGetROIsOnSimulation.m
+++ testCaImGetROIsOnSimulation.m
@@ -6,7 +6,8 @@ function [out, truthMap, ROI] = testCaImGetROIsOnSimulation(params)
 % Inputs:
 %   params - [optional] structure defining spatial image size (sz), signal
 %   duration (dur), number of ROI (nROI), signal to noise ratio (snr),
-%   noise spectrum type (noisePW), and name of file to save simulation to
+%   noise spectrum type (noisePW), cutoff or tolerances (cutoff) for segmentation
+%   algorithms and name of file to save simulation to
 %   (svMovie) for the simulation. Defaults are used otherwise.
 % Outputs:
 %   out - struct with hit and FA rates, d-prime values,... for each cutoff
@@ -23,7 +24,7 @@ if ~exist('params', 'var')
     dur = 10; % seconds sampled at 30Hz
     nROI = 5; % number of cells
     snr = 10; % signal to noise ratio
-    noisePW = 0; % noise magnitude spectrum (1/freq^noisePW) (0 = white)
+    noisePW = 1; % noise magnitude spectrum (1/freq^noisePW) (0 = white)
     svMovie = ''; % filename to save movie as .tif
     cutoff = .5; % cutoff for hierarchical clustering
     estNeuronRadius = 5;
@@ -38,16 +39,20 @@ else
     estNeuronRadius = params.estNeuronRadius;
     cutoff = params.cutoff;
 end
-cutoff = [1e-2,1e-1,1e0,1e1,1e2];
+
 
 %% simulate data
 [ROI, full] = simulateCalcImg(sz, dur, nROI, snr, noisePW, svMovie);
 
 %% automatic segmenation algorithm
-% [finalBinaryImage,clusterData, detectedROI] = CaImGetROIs(full, estNeuronRadius, cutoff);
-[detectedROI, finalBinaryImage] = kmeans_ForJanis(full,estNeuronRadius,nROI);
+% choose a segmentation algorithm to run
+[finalBinaryImage,clusterData, detectedROI] = CaImGetROIs(full, estNeuronRadius, cutoff);
 
-%% truth map
+% To Run KMeans Algorithm
+%cutoff = [1e-2,1e-1,1e0,1e1,1e2];
+%[detectedROI, finalBinaryImage] = kmeans_CaImGetROIs(full,estNeuronRadius,nROI);
+
+%% truth map - >0 for signal pixels, 0 for noise only
 truthMap = zeros(sz);
 for i = 1:nROI
     truthMap(ROI(i).indices) = i; % if overlap the later index dominates
@@ -63,6 +68,7 @@ for t = 1:length(detectedROI) % length depends on number of cutoffs given
     colormap jet;
     
     for j = 1:length(detectedROI{t})
+        % draw the outline of the detected ROI
         sIdx = detectedROI{t}(j).indices;
         [r, c] = ind2sub([sz, sz], sIdx);
         if ~isempty(r)
@@ -85,17 +91,19 @@ for t = 1:length(detectedROI) % length depends on number of cutoffs given
     set(gca, 'YDir', 'reverse');
     
     
-    %% individual pixel analysis (test of noise removal process)
+    %% test pixels that made it through noise removal process
     hitPixels = sum(truthMap(:) > 0 & finalBinaryImage(:)) / sum(truthMap(:) > 0);
     faPixels = sum(truthMap(:) == 0 & finalBinaryImage(:)) / sum(truthMap(:) == 0);
     dprimePixels = norminv(hitPixels, 0, 1) - norminv(faPixels, 0, 1);
     
-    %% signal pixels (test of what made it through clustering)
+    %% test pixels that made it through clustering
     hitSignal = sum(truthMap(:) > 0 & detectedMap(:)) / sum(truthMap(:) > 0);
     faSignal = sum(truthMap(:) == 0 & detectedMap(:)) / sum(truthMap(:) == 0);
     dprimeSignal = norminv(hitSignal, 0, 1) - norminv(faSignal, 0, 1);
     
     %% ROI analysis
+    % ignore detected ROI that didn't match, they'll be evident in the
+    % false alarm rates of the above analysis
     foundROIs = [detectedROI{t}.trueROI];
     actualNROI = length(unique(truthMap(:))) - any(truthMap(:) == 0);
     nDetectedPerTrue = sum(foundROIs > 0) / actualNROI;
